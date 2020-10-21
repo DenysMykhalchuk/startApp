09b99de2cdb2f0e3effe14b5f7ee0844e6a62aa7
Denys Mykhalchuk <mykhalchukdenis96@gmail.com>
2020-10-22 00:53:58 +0300

    Added the first app

---
+++ FirstApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!"); 
+            Console.WriteLine(2+2);
+            var str = "Hello";
+            str += " world";
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
